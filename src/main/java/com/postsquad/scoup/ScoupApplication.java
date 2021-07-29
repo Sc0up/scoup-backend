49f37c3470bd442046f42b9b45520ff38f9d4dba
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-07-29 22:24:44 +0900

    init project

---
+++ src/main/java/com/postsquad/scoup/ScoupApplication.java
@@ -0,0 +1,13 @@
+package com.postsquad.scoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/postsquad/scoup/ScoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postsquad.scoup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
