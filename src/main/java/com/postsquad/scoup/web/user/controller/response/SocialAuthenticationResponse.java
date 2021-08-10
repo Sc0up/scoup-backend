34577715acfb25159b47de5138f3aadfc651e5db
janeljs <jisunlim818@gmail.com>
2021-08-10 13:12:10 +0900

    feat: 🎸 소셜 서비스 id값을 나타내는 필드 및 DTO 변환 메서드 추가
    - socialServiceId 필드는 String으로 선언
    
    ✅ Closes: #12

--- src/main/java/com/postsquad/scoup/web/user/controller/response/SocialAuthenticationResponse.java
+++ src/main/java/com/postsquad/scoup/web/user/controller/response/SocialAuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.postsquad.scoup.web.user.controller.response;
 
+import com.postsquad.scoup.web.user.auth.OAuthUserResponse;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +8,17 @@ import lombok.Getter;
 @Builder
 public class SocialAuthenticationResponse {
 
+    private String socialServiceId;
     private String username;
-
     private String email;
-
     private String avatarUrl;
+
+    public static SocialAuthenticationResponse from(OAuthUserResponse oAuthUserResponse) {
+        return SocialAuthenticationResponse.builder()
+                .socialServiceId(String.valueOf(oAuthUserResponse.getId()))
+                .username(oAuthUserResponse.getLogin())
+                .email(oAuthUserResponse.getEmail())
+                .avatarUrl(oAuthUserResponse.getAvatarUrl())
+                .build();
+    }
 }
