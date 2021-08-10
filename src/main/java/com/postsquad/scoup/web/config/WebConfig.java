4215d39e82c07accdd87228f251bea95115b435c
janeljs <jisunlim818@gmail.com>
2021-08-10 14:52:16 +0900

    feat: 🎸 WebClient에 ObjectMapper 등록
    - snake case strategy를 사용하는 object mapper 등록
    
    ✅ Closes: #12

--- src/main/java/com/postsquad/scoup/web/config/WebConfig.java
+++ src/main/java/com/postsquad/scoup/web/config/WebConfig.java
@@ -1,5 +1,9 @@
 package com.postsquad.scoup.web.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +23,12 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public WebClient webClient(HttpClient httpClient) {
-        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+    public WebClient webClient(HttpClient httpClient, ObjectMapper baseConfig) {
+        ObjectMapper objectMapper = baseConfig.copy();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper)))
+                .build();
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).exchangeStrategies(exchangeStrategies).build();
     }
 }
--- src/main/java/com/postsquad/scoup/web/user/auth/response/OAuthUserResponse.java
+++ src/main/java/com/postsquad/scoup/web/user/auth/response/OAuthUserResponse.java
@@ -1,11 +1,8 @@
 package com.postsquad.scoup.web.user.auth.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 
 @Getter
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OAuthUserResponse {
 
     private Long id;
