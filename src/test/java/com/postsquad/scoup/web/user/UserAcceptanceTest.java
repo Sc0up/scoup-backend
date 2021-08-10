5fcb228a0427684db9153234284a125a069cf994
janeljs <jisunlim818@gmail.com>
2021-08-10 11:24:20 +0900

    fix: 🐛 Access token 발급 및 ObjectMapper 관련 문제 해결
    - OAuth 로그인 시 사용자 정보 입력 불가로 인해 응답의 location header value가 null로 반환되는 문제
    발생. 클라이언트에서 Authorization code를 전송해주면 해결되는 문제지만 아직 해당 기능이 구현되지 않음.
    - 백엔드 단에서 문제를 해결하기 위해 personal access token을 발급하여 테스트를 진행함.
    - RestAssured용 ObjectMapper를 등록하여 snake case 필드를 읽어오지 못하는 문제 해결.
    
    ✅ Closes: #12

--- src/test/java/com/postsquad/scoup/web/user/UserAcceptanceTest.java
+++ src/test/java/com/postsquad/scoup/web/user/UserAcceptanceTest.java
@@ -1,42 +1,46 @@
 package com.postsquad.scoup.web.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.postsquad.scoup.web.AcceptanceTestBase;
 import com.postsquad.scoup.web.user.controller.response.SocialAuthenticationResponse;
+import io.restassured.internal.mapping.Jackson2Mapper;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 
 class UserAcceptanceTest extends AcceptanceTestBase {
+
+    @Value("${github.access.token}")
+    private String accessToken;
+
+    @Autowired
+    private ObjectMapper objectMapperInner;
+
+    private final io.restassured.mapper.ObjectMapper objectMapper = new Jackson2Mapper((type, charset) -> objectMapperInner);
 
     @DisplayName("소셜 서비스에 가입된 사용자의 정보를 불러올 수 있다.")
     @ParameterizedTest
     @MethodSource("readUserDataProvider")
     void readUserData(SocialAuthenticationResponse expectedSocialAuthenticationResponse) {
         // given
-        String gitHubBaseUri = "https://github.com";
-        String gitHubPath = "/login/oauth/authorize?client_id=";
-        String clientId = "2abdddfdc8e14727fc07";
-        String authorizationCode = given()
-                .baseUri(gitHubBaseUri)
-                .basePath(gitHubPath)
-                .queryParam("client_id", clientId)
-                .get()
-                .asString();
-
-        String socialAuthenticationPath = "/api/social/authenticate";
+        String socialAuthenticationPath = "/api/social/authenticate/token";
         RequestSpecification givenRequest = given()
                 .baseUri(BASE_URL)
                 .port(port)
                 .basePath(socialAuthenticationPath)
-                .queryParam("authorization_code", authorizationCode);
+                .header(HttpHeaders.AUTHORIZATION, "token " + accessToken);
 
         // when
         Response actualResponse = givenRequest.when()
@@ -45,10 +49,9 @@ class UserAcceptanceTest extends AcceptanceTestBase {
                 .andReturn();
 
         // then
-        then(actualResponse)
+        assertThat(actualResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        then(actualResponse.as(SocialAuthenticationResponse.class, objectMapper))
                 .as("사용자의 소셜 계정 개인 정보 조회")
-                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.OK.value())
-                .extracting(response -> response.as(SocialAuthenticationResponse.class))
                 .usingRecursiveComparison()
                 .isEqualTo(expectedSocialAuthenticationResponse);
     }
@@ -56,6 +59,7 @@ class UserAcceptanceTest extends AcceptanceTestBase {
     private static Stream<SocialAuthenticationResponse> readUserDataProvider() {
         return Stream.of(SocialAuthenticationResponse
                 .builder()
+                .socialServiceId(68000537L)
                 .username("janeljs")
                 .email("jisunlim818@gmail.com")
                 .avatarUrl("https://avatars.githubusercontent.com/u/68000537?v=4")
