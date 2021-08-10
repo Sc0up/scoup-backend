43474ca2d3ac0269f05077f517212a4ef0abc630
janeljs <jisunlim818@gmail.com>
2021-08-10 15:33:18 +0900

    feat: 🎸 BaseEntity 클래스 추가
    - 생성 시간 및 수정 시간을 자동화하기 위해 JpaAuditing 적용
    
    ✅ Closes: #15

---
+++ src/main/java/com/postsquad/scoup/web/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.postsquad.scoup.web;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
