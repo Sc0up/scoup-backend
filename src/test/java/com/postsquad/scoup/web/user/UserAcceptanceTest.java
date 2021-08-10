62cc36065c269f12b3103f60be6cd9d9ac2498b9
Sally Oh <min27604@hotmail.com>
2021-08-11 00:55:46 +0900

    test: 💍 예외 처리를 위한 이메일 중복확인 인수 테스트 추가
    공백, null, 이메일 형식을 확인한다.
    
    ✅ Closes: #11

--- src/test/java/com/postsquad/scoup/web/user/UserAcceptanceTest.java
+++ src/test/java/com/postsquad/scoup/web/user/UserAcceptanceTest.java
@@ -1,6 +1,7 @@
 package com.postsquad.scoup.web.user;
 
 import com.postsquad.scoup.web.AcceptanceTestBase;
+import com.postsquad.scoup.web.error.controller.response.ErrorResponse;
 import com.postsquad.scoup.web.user.controller.response.EmailValidationResponse;
 import com.postsquad.scoup.web.user.repository.UserRepository;
 import io.restassured.RestAssured;
@@ -14,6 +15,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -63,5 +65,56 @@ public class UserAcceptanceTest extends AcceptanceTestBase {
                         "notExisting@email.com",
                         EmailValidationResponse.builder().existingEmail(false).build()
         ));
+    }
+
+    @ParameterizedTest
+    @MethodSource("validateRequestParamProvider")
+    @DisplayName("Request parameter validation")
+    void validateRequestParam(String description, String givenEmailRequest, ErrorResponse expectedResponse) {
+        String path = "/api/validate/email";
+        RequestSpecification givenRequest = RestAssured.given()
+                                                    .baseUri(BASE_URL)
+                                                    .port(port)
+                                                    .basePath(path)
+                                                    .queryParam("email", givenEmailRequest);
+
+        Response actualResponse = givenRequest.when()
+                                          .log().all()
+                                          .get()
+                                          .andReturn();
+
+        then(actualResponse)
+                .as("이메일 중복 확인: %s", description)
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.BAD_REQUEST.value())
+                .extracting(response -> response.as(ErrorResponse.class, restAssuredObjectMapper))
+                .usingRecursiveComparison()
+                .ignoringFields("timestamp")
+                .isEqualTo(expectedResponse);
+    }
+
+    static Stream<Arguments> validateRequestParamProvider() {
+        return Stream.of(
+                Arguments.of("실패: 빈 문자열",
+                        "",
+                        ErrorResponse.builder()
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .message("Bad Request")
+                                .errors(Collections.singletonList("validateEmail.email: must not be empty"))
+                                .build()),
+                Arguments.of("실패: null",
+                        null,
+                        ErrorResponse.builder()
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .message("Bad Request")
+                                .errors(Collections.singletonList("validateEmail.email: must not be empty"))
+                                .build()),
+                Arguments.of("실패: 이메일 형식",
+                        "email",
+                        ErrorResponse.builder()
+                                .statusCode(HttpStatus.BAD_REQUEST.value())
+                                .message("Bad Request")
+                                .errors(Collections.singletonList("validateEmail.email: must be a well-formed email address"))
+                                .build())
+        );
     }
 }
