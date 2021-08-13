b6c011e762189c7a61fa6f9e1c58a7c40133bd23
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-08-13 18:59:32 +0900

    refactor: 💡 불필요한 설정 제거
    builder.gradle에 Mastruct, Lombok 관련 설정으로 어노테이션 설정
    필요없어짐.
    
    ✅ Closes: #14

--- src/main/java/com/postsquad/scoup/web/user/mapper/UserMapper.java
+++ src/main/java/com/postsquad/scoup/web/user/mapper/UserMapper.java
@@ -2,11 +2,10 @@ package com.postsquad.scoup.web.user.mapper;
 
 import com.postsquad.scoup.web.user.controller.request.SignUpRequest;
 import com.postsquad.scoup.web.user.domain.User;
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
