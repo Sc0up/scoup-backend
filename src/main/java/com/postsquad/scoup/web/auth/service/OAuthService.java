9674e09712157b05a373c7a49e7515669c131512
janeljs <jisunlim818@gmail.com>
2021-08-16 19:27:05 +0900

    feat: 🎸 Google, Kakao, GitHub을 구분하는 type을 메서드 파라미터에 추가
    - readOAuthUserData와 readOAuthUserDataFromHeader 메서드에 소셜 서비스의 종류를 나타내는 파라미터 추가
    - GitHubOAuth에 있던 로직을 OAuthService로 이동
    
    GitHubOAuth가 메인 서비스의 역할을 하고 있고, OAuthService는 하는 역할이 거의 없다고 판단되어 GitHubOAuth의 로직을 OAuthService로 통합하였음
    
    ✅ Closes: #68

--- src/main/java/com/postsquad/scoup/web/auth/service/OAuthService.java
+++ src/main/java/com/postsquad/scoup/web/auth/service/OAuthService.java
@@ -1,26 +1,73 @@
 package com.postsquad.scoup.web.auth.service;
 
+import com.postsquad.scoup.web.auth.controller.request.AccessTokenRequest;
 import com.postsquad.scoup.web.auth.controller.response.AccessTokenResponse;
 import com.postsquad.scoup.web.auth.controller.response.OAuthUserResponse;
 import com.postsquad.scoup.web.auth.controller.response.SocialAuthenticationResponse;
+import com.postsquad.scoup.web.auth.exception.AccessTokenNotFoundException;
+import com.postsquad.scoup.web.auth.exception.GitHubRequestNotValidException;
+import com.postsquad.scoup.web.auth.exception.GitHubUserNotFoundException;
+import com.postsquad.scoup.web.auth.exception.OAuthException;
+import com.postsquad.scoup.web.auth.property.OAuthProperties;
+import com.postsquad.scoup.web.auth.property.OAuthProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Service
 public class OAuthService {
 
-    private final OAuth oauth;
+    public static final String TOKEN = "token";
+    private final WebClient webClient;
+    private final OAuthProperties oAuthProperties;
 
-    public SocialAuthenticationResponse readOAuthUserData(String code) {
-        AccessTokenResponse token = oauth.getToken(code);
-        OAuthUserResponse oAuthUserInfo = oauth.getOAuthUserInfo(token.getAccessToken());
+    public SocialAuthenticationResponse readOAuthUserData(String type, String code) {
+        OAuthProperty property = oAuthProperties.getProperty(type);
+        AccessTokenResponse token = getToken(property, code);
+        OAuthUserResponse oAuthUserInfo = getOAuthUserInfo(property, token.getAccessToken());
         return SocialAuthenticationResponse.from(oAuthUserInfo);
     }
 
-    public SocialAuthenticationResponse readOAuthUserDataWithToken(String header) {
-        OAuthUserResponse oAuthUserInfo = oauth.getOAuthUserInfoFromHeader(header);
+    public SocialAuthenticationResponse readOAuthUserDataFromHeader(String type, String header) {
+        OAuthProperty property = oAuthProperties.getProperty(type);
+        String accessToken = header.substring(TOKEN.length()).trim();
+        OAuthUserResponse oAuthUserInfo = getOAuthUserInfo(property, accessToken);
         return SocialAuthenticationResponse.from(oAuthUserInfo);
+    }
+
+    private AccessTokenResponse getToken(OAuthProperty oAuthProperty, String code) {
+        AccessTokenRequest accessTokenRequest = AccessTokenRequest.builder()
+                                                                  .clientId(oAuthProperty.getClientId())
+                                                                  .clientSecret(oAuthProperty.getClientSecret())
+                                                                  .code(code)
+                                                                  .build();
+
+        return webClient.post()
+                        .uri(oAuthProperty.getAccessTokenUri())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .bodyValue(accessTokenRequest)
+                        .retrieve()
+                        .onStatus(HttpStatus::is4xxClientError, error -> Mono.error(() -> new OAuthException(new GitHubRequestNotValidException())))
+                        .bodyToMono(AccessTokenResponse.class)
+                        .blockOptional()
+                        .orElseThrow(() -> new OAuthException(new AccessTokenNotFoundException()));
+    }
+
+
+    private OAuthUserResponse getOAuthUserInfo(OAuthProperty oAuthProperty, String accessToken) {
+        return webClient.get()
+                        .uri(oAuthProperty.getUserUri())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, TOKEN + " " + accessToken)
+                        .retrieve()
+                        .onStatus(HttpStatus::is4xxClientError, error -> Mono.error(() -> new OAuthException(new GitHubRequestNotValidException())))
+                        .bodyToMono(OAuthUserResponse.class)
+                        .blockOptional()
+                        .orElseThrow(() -> new OAuthException(new GitHubUserNotFoundException()));
     }
 }
