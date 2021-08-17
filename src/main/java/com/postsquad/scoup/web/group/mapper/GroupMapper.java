f769b8354a8c4b61250b3bf6d668bb9be68b00ff
Sally Oh <min27604@hotmail.com>
2021-08-17 16:37:44 +0900

    feat: 🎸 그룹 매퍼 생성
    - 그룹 엔티티와 GroupCreationRequest 사이를 매핑해주는 메서드 구현
    
    ✅ Closes: #72

--- src/main/java/com/postsquad/scoup/web/group/mapper/GroupMapper.java
+++ src/main/java/com/postsquad/scoup/web/group/mapper/GroupMapper.java
@@ -1,2 +1,14 @@
-package com.postsquad.scoup.web.group.mapper;public class GroupMapper {
+package com.postsquad.scoup.web.group.mapper;
+
+import com.postsquad.scoup.web.group.controller.request.GroupCreationRequest;
+import com.postsquad.scoup.web.group.domain.Group;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface GroupMapper {
+
+    GroupMapper INSTANCE = Mappers.getMapper(GroupMapper.class);
+
+    Group groupCreationRequestToGroup(GroupCreationRequest groupCreationRequest);
 }
