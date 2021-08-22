cac2733f517980d6e14dfe49fcd05c97e561e976
janeljs <jisunlim818@gmail.com>
2021-08-23 08:28:48 +0900

    feat: 🎸 Mapstruct를 이용한 SocialAuthenticationResponseMapper 추가
    ✅ Closes: #68

---
+++ src/main/java/com/postsquad/scoup/web/auth/controller/response/SocialAuthenticationResponseMapper.java
@@ -0,0 +1,39 @@
+package com.postsquad.scoup.web.auth.controller.response;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface SocialAuthenticationResponseMapper {
+
+    SocialAuthenticationResponseMapper INSTANCE = Mappers.getMapper(SocialAuthenticationResponseMapper.class);
+
+    @Mappings({
+            @Mapping(target = "oAuthType", source = "type"),
+            @Mapping(target = "socialServiceId", source = "gitHubUserResponse.id"),
+            @Mapping(target = "username", source = "gitHubUserResponse.login"),
+            @Mapping(target = "email", source = "gitHubUserResponse.email"),
+            @Mapping(target = "avatarUrl", source = "gitHubUserResponse.avatarUrl")
+    })
+    SocialAuthenticationResponse GitHubUserResponseToSocialAuthenticationResponse(GitHubUserResponse gitHubUserResponse, String type);
+
+    @Mappings({
+            @Mapping(target = "oAuthType", source = "type"),
+            @Mapping(target = "socialServiceId", source = "kakaoUserResponse.id"),
+            @Mapping(target = "username", source = "kakaoUserResponse.kakaoAccount.profile.nickname"),
+            @Mapping(target = "email", source = "kakaoUserResponse.kakaoAccount.email"),
+            @Mapping(target = "avatarUrl", source = "kakaoUserResponse.kakaoAccount.profile.profileImageUrl")
+    })
+    SocialAuthenticationResponse KakaoUserResponseToSocialAuthenticationResponse(KakaoUserResponse kakaoUserResponse, String type);
+
+    @Mappings({
+            @Mapping(target = "oAuthType", source = "type"),
+            @Mapping(target = "socialServiceId", source = "googleUserResponse.id"),
+            @Mapping(target = "username", source = "googleUserResponse.name"),
+            @Mapping(target = "email", source = "googleUserResponse.email"),
+            @Mapping(target = "avatarUrl", source = "googleUserResponse.picture")
+    })
+    SocialAuthenticationResponse GoogleUserResponseToSocialAuthenticationResponse(GoogleUserResponse googleUserResponse, String type);
+}
