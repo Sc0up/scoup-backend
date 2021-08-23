f0759e8c402a85634ff7e8d3436d14e64bb5df6c
janeljs <jisunlim818@gmail.com>
2021-08-23 09:29:23 +0900

    feat: 🎸 OAuth 설정 파일과 인가 코드를 받아 토큰 요청을 보내는 DTO 생성 메서드 추가
    ✅ Closes: #68

--- src/main/java/com/postsquad/scoup/web/auth/controller/request/AccessTokenRequestWithGrantType.java
+++ src/main/java/com/postsquad/scoup/web/auth/controller/request/AccessTokenRequestWithGrantType.java
@@ -8,14 +8,16 @@ import org.springframework.util.MultiValueMap;
 @Getter
 public class AccessTokenRequestWithGrantType {
 
-    public static final String GRANT_TYPE = "authorization_code";
+    private static final String GRANT_TYPE = "authorization_code";
+
     private static final MultiValueMap<String, String> accessTokenRequest = new LinkedMultiValueMap<>();
 
-    public AccessTokenRequestWithGrantType(OAuthProperty oAuthProperty, String code) {
+    public static MultiValueMap<String, String> of(OAuthProperty oAuthProperty, String code) {
         accessTokenRequest.add("grant_type", GRANT_TYPE);
         accessTokenRequest.add("client_id", oAuthProperty.getClientId());
         accessTokenRequest.add("redirect_uri", oAuthProperty.getRedirectUri());
         accessTokenRequest.add("client_secret", oAuthProperty.getClientSecret());
         accessTokenRequest.add("code", code);
+        return accessTokenRequest;
     }
 }
