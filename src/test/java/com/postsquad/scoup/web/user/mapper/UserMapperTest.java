e46915902934ee7b5c484d7672714a272c0eabac
janeljs <jisunlim818@gmail.com>
2021-08-30 19:09:36 +0900

    test: 💍 UserMapperTest 테스트 데이터 수정
    OAuthInfo 추가로 인해 추가된 oAuthType, socialServiceId, avatarUrl 필드를 테스트 데이터에
    추가함.
    
    ✅ Closes: #6

--- src/test/java/com/postsquad/scoup/web/user/mapper/UserMapperTest.java
+++ src/test/java/com/postsquad/scoup/web/user/mapper/UserMapperTest.java
@@ -1,11 +1,14 @@
 package com.postsquad.scoup.web.user.mapper;
 
+import com.postsquad.scoup.web.auth.OAuthType;
 import com.postsquad.scoup.web.user.controller.request.SignUpRequest;
+import com.postsquad.scoup.web.user.domain.OAuthInfo;
 import com.postsquad.scoup.web.user.domain.User;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -28,16 +31,21 @@ class UserMapperTest {
         return Stream.of(
                 Arguments.arguments(
                         SignUpRequest.builder()
+                                     .oAuthType(OAuthType.GITHUB)
+                                     .socialServiceId("1234567")
                                      .nickname("nickname")
                                      .username("username")
-                                     .email("email")
+                                     .email("email@email")
                                      .password("password")
+                                     .avatarUrl("https://avatars.githubusercontent.com/u/68000537?v=4")
                                      .build(),
                         User.builder()
                             .nickname("nickname")
                             .username("username")
-                            .email("email")
+                            .email("email@email")
                             .password("password")
+                            .avatarUrl("https://avatars.githubusercontent.com/u/68000537?v=4")
+                            .oAuthInfo(List.of(new OAuthInfo(OAuthType.GITHUB, "1234567")))
                             .build()
                 )
         );
