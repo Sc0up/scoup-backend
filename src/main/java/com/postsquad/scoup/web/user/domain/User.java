6224b8afa167f5d6c7db4eba5b995c9ab0c3b8b1
janeljs <jisunlim818@gmail.com>
2021-08-30 18:56:25 +0900

    feat: 🎸 User 클래스에 oAuthInfo 컬렉션 추가
    테스트에서 deleteAll() 메서드를 실행할 때 제약 조건을 지키지 않고 있다는 에러 발생을 막기 위해 `@OnDelete`
    어노테이션을 추가함.
    
    ✅ Closes: #6

--- src/main/java/com/postsquad/scoup/web/user/domain/User.java
+++ src/main/java/com/postsquad/scoup/web/user/domain/User.java
@@ -1,15 +1,13 @@
 package com.postsquad.scoup.web.user.domain;
 
 import com.postsquad.scoup.web.common.BaseEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -35,16 +33,24 @@ public class User extends BaseEntity {
     @Column(length = 30, nullable = false)
     private String password;
 
-    protected User(String nickname, String username, String email, String avatarUrl, String password) {
+    @ElementCollection(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id")
+    private List<OAuthInfo> oAuthInfo = new ArrayList<>();
+
+    protected User(String nickname, String username, String email, String avatarUrl, String password, List<OAuthInfo> oAuthInfo) {
         this.nickname = nickname;
         this.username = username;
         this.email = email;
         this.avatarUrl = avatarUrl;
         this.password = password;
+        if (oAuthInfo != null) {
+            this.oAuthInfo = oAuthInfo;
+        }
     }
 
     @Builder
-    public static User of(String nickname, String username, String email, String avatarUrl, String password) {
-        return new User(nickname, username, email, avatarUrl, password);
+    public static User of(String nickname, String username, String email, String avatarUrl, String password, List<OAuthInfo> oAuthInfo) {
+        return new User(nickname, username, email, avatarUrl, password, oAuthInfo);
     }
 }
