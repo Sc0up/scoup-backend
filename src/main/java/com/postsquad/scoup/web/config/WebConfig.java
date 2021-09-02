47d83587f0e349da3c0afe90ade989fd3f17f0fe
Sally Oh <min27604@hotmail.com>
2021-09-03 00:56:43 +0900

    feat: 🎸 UserArgumentResolver 적용
    ✅ Closes: #105

--- src/main/java/com/postsquad/scoup/web/config/WebConfig.java
+++ src/main/java/com/postsquad/scoup/web/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.postsquad.scoup.web.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.postsquad.scoup.web.user.UserArgumentResolver;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reactor.netty.http.client.HttpClient;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -27,5 +31,10 @@ public class WebConfig implements WebMvcConfigurer {
                                                                   .codecs(configurer -> configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(baseConfig)))
                                                                   .build();
         return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient)).exchangeStrategies(exchangeStrategies).build();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserArgumentResolver());
     }
 }
