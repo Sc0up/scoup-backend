5cfa4037901e66964dbdc38c38e12d0da4932da3
Sally Oh <min27604@hotmail.com>
2021-09-03 01:08:55 +0900

    test: 💍 그룹 인수테스트 수정
    - 헤더에 임의로 만들어 둔 토큰을 적용할 수 있도록 수정
    - 그룹 생성 시 오너 추가
    
    ✅ Closes: #105

--- src/test/java/com/postsquad/scoup/web/group/GroupAcceptanceTest.java
+++ src/test/java/com/postsquad/scoup/web/group/GroupAcceptanceTest.java
@@ -5,6 +5,8 @@ import com.postsquad.scoup.web.error.controller.response.ErrorResponse;
 import com.postsquad.scoup.web.group.controller.request.GroupCreationRequest;
 import com.postsquad.scoup.web.group.domain.Group;
 import com.postsquad.scoup.web.group.repository.GroupRepository;
+import com.postsquad.scoup.web.user.domain.User;
+import com.postsquad.scoup.web.user.repository.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -29,12 +31,26 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
     @Autowired
     GroupRepository groupRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
+    // temporary token with sub(userId) = 1, exp = 2023-01-01T00:00:00.000(Korean Standard Time)
+    private static String TEST_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNjcyNDk4ODAwfQ.DXojMeUGIq77XWvQK0luZtZhsi-c6s9qjiiu9vHhkbg";
+    private static User TEST_USER = User.builder()
+                                        .nickname("nickname")
+                                        .email("email@email.com")
+                                        .password("password")
+                                        .avatarUrl("url")
+                                        .username("username")
+                                        .build();
+
     @BeforeEach
     void setUp() {
         groupRepository.save(Group.builder()
                                   .name("name")
                                   .description("description")
                                   .build());
+        userRepository.save(TEST_USER);
     }
 
     @AfterEach
@@ -53,6 +69,8 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
                                                        .port(port)
                                                        .basePath(path)
                                                        .contentType(ContentType.JSON)
+                                                       .header("Accept-Language", "en-US")
+                                                       .header("Authorization", TEST_TOKEN)
                                                        .body(givenGroupCreationRequest);
 
         // when
@@ -82,7 +100,9 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
                         Group.builder()
                              .name("group name")
                              .description("description")
-                             .build()
+                             .owner(TEST_USER)
+                             .build(),
+                        TEST_USER
                 )
         );
     }
@@ -99,6 +119,7 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
                                                        .basePath(path)
                                                        .contentType(ContentType.JSON)
                                                        .header("Accept-Language", "en-US")
+                                                       .header("Authorization", TEST_TOKEN)
                                                        .body(givenGroupCreationRequest);
 
         // when
@@ -145,6 +166,7 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
                                                        .basePath(path)
                                                        .contentType(ContentType.JSON)
                                                        .header("Accept-Language", "en-US")
+                                                       .header("Authorization", TEST_TOKEN)
                                                        .body(givenGroupCreationRequest);
 
         // when
@@ -179,4 +201,58 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
                 )
         );
     }
+
+    // 일단 엔티티에 오너 추가
+    // 그룹 생성시에 토큰에서 유저아이디 꺼내서 오너 등록
+    // 토큰에 있는 유저 아이디로 유저 조회
+    // 수정할 그룹의 오너인지 확인
+    // 그룹 수정
+//    @ParameterizedTest
+//    @MethodSource("modifyGroupProvider")
+//    @DisplayName("사용자가 그룹을 수정 할 수 있다")
+//    void modifyGroup(String description, GroupModificationRequest givenGroupModificationRequest, Group expectedGroup) {
+//        // given
+//        // TODO: 헤더에 토큰 필요
+//        String path = "/api/group";
+//        RequestSpecification givenRequest = RestAssured.given()
+//                                                       .baseUri(BASE_URL)
+//                                                       .port(port)
+//                                                       .basePath(path)
+//                                                       .contentType(ContentType.JSON)
+//                                                       .header("Accept-Language", "en-US")
+//                                                       .header("Authorization", TEST_TOKEN)
+//                                                       .body(givenGroupModificationRequest);
+//
+//        // when
+//        Response actualResponse = givenRequest.when()
+//                                              .log().all()
+//                                              .put();
+//
+//        // then
+//        // TODO: 뭘 리턴하지? 일단은 수정된 그룹의 아이디를 리턴한다고 가정
+//        actualResponse.then()
+//                      .log().all()
+//                      .statusCode(HttpStatus.NO_CONTENT.value());
+//        then(groupRepository.findById(actualResponse.as(Long.class)).orElse(null))
+//                .as("그룹 수정: %s" + description)
+//                .usingRecursiveComparison()
+//                .ignoringFields(ignoringFieldsForResponse)
+//                .isEqualTo(expectedGroup);
+//    }
+//
+//    static Stream<Arguments> modifyGroupProvider() {
+//        return Stream.of(
+//                Arguments.of(
+//                        "성공",
+//                        GroupModificationRequest.builder()
+//                                                .name("modifiedName")
+//                                                .description("modified description")
+//                                                .build(),
+//                        Group.builder()
+//                             .name("modifiedName")
+//                             .description("modified description")
+//                             .build()
+//                )
+//        );
+//    }
 }
