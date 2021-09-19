e6670d3f2ab5a8f8fe5d325265f2845d4a80a7a0
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-09-19 23:28:38 +0900

    fix: 🐛 ScheduleCandidateReadAllResponses JSON property 이름 수정
    ✅ Closes: #120

--- src/main/java/com/postsquad/scoup/web/schedule/controller/response/ScheduleCandidateReadAllResponses.java
+++ src/main/java/com/postsquad/scoup/web/schedule/controller/response/ScheduleCandidateReadAllResponses.java
@@ -1,5 +1,6 @@
 package com.postsquad.scoup.web.schedule.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ import java.util.Map;
 @Data
 public class ScheduleCandidateReadAllResponses {
 
+    @JsonProperty("scheduleCandidates")
     private Map<LocalDate, List<ScheduleCandidateReadAllResponse>> scheduleCandidateResponses;
 }
