52633a3d9b4330acd8dd93bd9115ba9a04c8e382
Dae-Hwa <DaeHwa_Jeong@outlook.com>
2021-09-26 01:27:47 +0900

    fix: 🐛 환경변수 생성 스크립트에서 암호 노출 되지 않도록 수정
    ✅ Closes: #143

--- auth-key-properties-creator.sh
+++ auth-key-properties-creator.sh
@@ -3,7 +3,6 @@
 for _AUTH_KEY in $AUTH_KEY; do
 
   IFS='=' read -ra VALUES <<<"$_AUTH_KEY"
-  echo "${VALUES[@]}"
   if [ "${VALUES[0]}" = "jasypt.encryptor.password" ]; then
     export JASYPT_ENCRYPTOR_PASSWORD="${VALUES[1]}"
   elif [ "${VALUES[0]}" = "jwt.secret.mac" ]; then
