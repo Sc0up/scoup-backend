6ee50a6ec6d8c5d0a3e7b3ba0bb91019f0433dd2
janeljs <jisunlim818@gmail.com>
2021-09-27 07:05:22 +0900

    feat: 🎸 ConfirmedScheduleTestDataSetup 클래스 추가
    TestDataSetup 클래스에서 확정 일정 조회에 관련된 데이터 분리
    
    ✅ Closes: #169

--- src/test/java/com/postsquad/scoup/web/TestDataSetup.java
+++ src/test/java/com/postsquad/scoup/web/TestDataSetup.java
@@ -1,9 +1,6 @@
 package com.postsquad.scoup.web;
 
 import com.postsquad.scoup.web.auth.OAuthType;
-import com.postsquad.scoup.web.group.domain.Group;
-import com.postsquad.scoup.web.schedule.domain.ConfirmedSchedule;
-import com.postsquad.scoup.web.schedule.domain.Schedule;
 import com.postsquad.scoup.web.user.domain.OAuthUser;
 import com.postsquad.scoup.web.user.domain.User;
 import org.springframework.stereotype.Service;
@@ -11,8 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,29 +26,6 @@ public class TestDataSetup {
                         .username("username")
                         .oAuthUsers(List.of(OAuthUser.of(OAuthType.NONE, "")))
                         .build();
-
-        Group group = Group.builder()
-                           .name("name")
-                           .description("")
-                           .schedules(new ArrayList<>())
-                           .owner(user)
-                           .build();
-
-        Schedule schedule = Schedule.builder()
-                                    .group(group)
-                                    .title("schedule title")
-                                    .description("schedule description")
-                                    .dueDateTime(LocalDateTime.of(2021, 9, 23, 0, 0))
-                                    .build();
-        group.addSchedule(schedule);
-
-        ConfirmedSchedule confirmedSchedule = ConfirmedSchedule.builder()
-                                                               .colorCode("#CAB8FF")
-                                                               .startDateTime(LocalDateTime.of(2021, 9, 25, 9, 0))
-                                                               .endDateTime(LocalDateTime.of(2021, 9, 25, 11, 0))
-                                                               .confirmedParticipants(List.of(user))
-                                                               .build();
-        schedule.setConfirmedSchedule(confirmedSchedule);
-        entityManager.persist(group);
+        entityManager.persist(user);
     }
 }
