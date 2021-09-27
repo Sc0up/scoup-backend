3ce14eb17ccfa51a0e0a0ddb48837f7e0da99224
janeljs <jisunlim818@gmail.com>
2021-09-27 11:27:18 +0900

    refactor: 💡 GroupAcceptanceTest 클래스 리팩토링
    Repository 주입 대신 testEntityManager를 사용하는 방식으로 변경
    
    ✅ Closes: #169

--- src/test/java/com/postsquad/scoup/web/group/GroupAcceptanceTest.java
+++ src/test/java/com/postsquad/scoup/web/group/GroupAcceptanceTest.java
@@ -1,7 +1,7 @@
 package com.postsquad.scoup.web.group;
 
 import com.postsquad.scoup.web.AcceptanceTestBase;
-import com.postsquad.scoup.web.auth.OAuthType;
+import com.postsquad.scoup.web.TestEntityManager;
 import com.postsquad.scoup.web.error.controller.response.ErrorResponse;
 import com.postsquad.scoup.web.group.controller.request.GroupCreationRequest;
 import com.postsquad.scoup.web.group.controller.request.GroupModificationRequest;
@@ -10,15 +10,10 @@ import com.postsquad.scoup.web.group.provider.CreateGroupProvider;
 import com.postsquad.scoup.web.group.provider.CreateGroupWithExistingNameProvider;
 import com.postsquad.scoup.web.group.provider.ModifyGroupProvider;
 import com.postsquad.scoup.web.group.provider.ValidateGroupCreationRequestProvider;
-import com.postsquad.scoup.web.group.repository.GroupRepository;
-import com.postsquad.scoup.web.user.domain.OAuthUser;
-import com.postsquad.scoup.web.user.domain.User;
-import com.postsquad.scoup.web.user.repository.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -26,32 +21,18 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class GroupAcceptanceTest extends AcceptanceTestBase {
 
     @Autowired
-    GroupRepository groupRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
-    // temporary token with sub(userId) = 1, exp = 2023-01-01T00:00:00.000(Korean Standard Time)
-    private static String TEST_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNjcyNDk4ODAwfQ.DXojMeUGIq77XWvQK0luZtZhsi-c6s9qjiiu9vHhkbg";
-    private User testUser = User.builder()
-                                .nickname("nickname")
-                                .email("email@email.com")
-                                .password("password")
-                                .avatarUrl("url")
-                                .username("username")
-                                .oAuthUsers(List.of(OAuthUser.of(OAuthType.NONE, "")))
-                                .build();
+    TestEntityManager testEntityManager;
 
     @BeforeEach
     void setUp() {
-          userRepository.save(testUser);
+        testEntityManager.persist(testUser);
     }
 
     @ParameterizedTest
@@ -78,7 +59,8 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
         actualResponse.then()
                       .log().all()
                       .statusCode(HttpStatus.CREATED.value());
-        Group actualGroup = groupRepository.findById(actualResponse.body().as(Long.class)).orElse(null);
+
+        Group actualGroup = testEntityManager.findById(Group.class, actualResponse.body().as(Long.class));
         then(actualGroup)
                 .as("그룹 생성: %s", description)
                 .usingRecursiveComparison()
@@ -96,7 +78,7 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
     @DisplayName("중복된 그룹 이름이 있으면 그룹 생성을 할 수 없다.")
     void createGroupWithExistingName(String description, GroupCreationRequest givenGroupCreationRequest, ErrorResponse expectedResponse) {
         // given
-        groupRepository.save(Group.builder().name("name").description("").build());
+        testEntityManager.persist(Group.builder().name("name").description("").build());
         String path = "/api/groups";
         RequestSpecification givenRequest = RestAssured.given()
                                                        .baseUri(BASE_URL)
@@ -158,7 +140,8 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
     @DisplayName("사용자가 그룹을 수정 할 수 있다")
     void modifyGroup(String description, Long givenGroupId, GroupModificationRequest givenGroupModificationRequest, Group expectedGroup) {
         // given
-        groupRepository.save(Group.builder().name("name").description("").owner(testUser).build());
+        Group group = Group.builder().name("name").description("").owner(testUser).schedules(new ArrayList<>()).build();
+        testEntityManager.persist(group);
         String path = "/api/groups/";
         RequestSpecification givenRequest = RestAssured.given()
                                                        .baseUri(BASE_URL)
@@ -178,7 +161,7 @@ public class GroupAcceptanceTest extends AcceptanceTestBase {
         actualResponse.then()
                       .log().all()
                       .statusCode(HttpStatus.OK.value());
-        then(groupRepository.findById(actualResponse.as(Long.class)).orElse(null))
+        then(testEntityManager.findById(Group.class, actualResponse.as(Long.class)))
                 .as("그룹 수정: %s", description)
                 .usingRecursiveComparison()
                 .ignoringFields(ignoringFieldsForResponseWithId)
