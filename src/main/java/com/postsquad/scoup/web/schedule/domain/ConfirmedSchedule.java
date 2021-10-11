065cd70d9cc0c299a5273817f75c8b688f86f259
janeljs <jisunlim818@gmail.com>
2021-10-12 03:39:20 +0900

    feat: 🎸 확정 일정에서 색상 코드 필드 제거
    code review: https://github.com/Sc0up/scoup-backend/pull/180#discussion_r726117420
    
    ✅ Closes: #169

--- src/main/java/com/postsquad/scoup/web/schedule/domain/ConfirmedSchedule.java
+++ src/main/java/com/postsquad/scoup/web/schedule/domain/ConfirmedSchedule.java
@@ -3,7 +3,6 @@ package com.postsquad.scoup.web.schedule.domain;
 import com.postsquad.scoup.web.common.BaseEntity;
 import com.postsquad.scoup.web.user.domain.User;
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -15,14 +14,9 @@ import java.util.List;
 @Entity
 public class ConfirmedSchedule extends BaseEntity {
 
-    private static final String DEFAULT_COLOR_CODE = "#00ff0000";
-
     @OneToOne(mappedBy = "confirmedSchedule")
     @Setter
     private Schedule schedule;
-
-    @ColumnDefault("'" + DEFAULT_COLOR_CODE + "'")
-    private String colorCode;
 
     private LocalDateTime startDateTime;
 
@@ -32,16 +26,15 @@ public class ConfirmedSchedule extends BaseEntity {
     @JoinColumn
     List<User> confirmedParticipants = new ArrayList<>();
 
-    protected ConfirmedSchedule(Schedule schedule, String colorCode, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    protected ConfirmedSchedule(Schedule schedule, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         this.schedule = schedule;
-        this.colorCode = colorCode;
         this.startDateTime = startDateTime;
         this.endDateTime = endDateTime;
     }
 
     @Builder
-    public static ConfirmedSchedule of(Schedule schedule, String colorCode, LocalDateTime startDateTime, LocalDateTime endDateTime, @Singular List<User> confirmedParticipants) {
-        ConfirmedSchedule confirmedSchedule = new ConfirmedSchedule(schedule, colorCode, startDateTime, endDateTime);
+    public static ConfirmedSchedule of(Schedule schedule, LocalDateTime startDateTime, LocalDateTime endDateTime, @Singular List<User> confirmedParticipants) {
+        ConfirmedSchedule confirmedSchedule = new ConfirmedSchedule(schedule, startDateTime, endDateTime);
         confirmedSchedule.addConfirmedParticipants(confirmedParticipants);
         return confirmedSchedule;
     }
