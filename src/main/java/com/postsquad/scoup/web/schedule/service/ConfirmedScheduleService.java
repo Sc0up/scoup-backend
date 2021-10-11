d4100467643273b5fad9d4f005b36c23a3d30867
janeljs <jisunlim818@gmail.com>
2021-10-12 04:16:45 +0900

    refactor: 💡 확정 일정 리스트를 변수로 추출
    code review: https://github.com/Sc0up/scoup-backend/pull/180#discussion_r726124718
    
    ✅ Closes: #169

--- src/main/java/com/postsquad/scoup/web/schedule/service/ConfirmedScheduleService.java
+++ src/main/java/com/postsquad/scoup/web/schedule/service/ConfirmedScheduleService.java
@@ -1,11 +1,13 @@
 package com.postsquad.scoup.web.schedule.service;
 
+import com.postsquad.scoup.web.schedule.controller.response.ConfirmedScheduleReadAllResponse;
 import com.postsquad.scoup.web.schedule.controller.response.ConfirmedScheduleReadAllResponses;
 import com.postsquad.scoup.web.schedule.mapper.ConfirmedScheduleReadAllResponseMapper;
 import com.postsquad.scoup.web.schedule.repository.ConfirmedScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -15,9 +17,10 @@ public class ConfirmedScheduleService {
     private final ConfirmedScheduleRepository confirmedScheduleRepository;
 
     public ConfirmedScheduleReadAllResponses readAll(Long groupId) {
-        return ConfirmedScheduleReadAllResponses.from(confirmedScheduleRepository.findConfirmedSchedulesBySchedule_Group_Id(groupId)
-                                                                                 .stream()
-                                                                                 .map(ConfirmedScheduleReadAllResponseMapper.INSTANCE::map)
-                                                                                 .collect(Collectors.toList()));
+        List<ConfirmedScheduleReadAllResponse> confirmedSchedules = confirmedScheduleRepository.findConfirmedSchedulesBySchedule_Group_Id(groupId)
+                                                                                               .stream()
+                                                                                               .map(ConfirmedScheduleReadAllResponseMapper.INSTANCE::map)
+                                                                                               .collect(Collectors.toList());
+        return ConfirmedScheduleReadAllResponses.from(confirmedSchedules);
     }
 }
