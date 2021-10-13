92bb0615fd9ad0f3736de13f236b831a17860cb3
janeljs <jisunlim818@gmail.com>
2021-10-13 13:55:58 +0900

    docs: ✏️ Swagger API 문서를 위한 코드 추가
    ✅ Closes: #186

--- src/main/java/com/postsquad/scoup/ScoupApplication.java
+++ src/main/java/com/postsquad/scoup/ScoupApplication.java
@@ -1,9 +1,18 @@
 package com.postsquad.scoup;
 
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableJpaAuditing
 @SpringBootApplication
 public class ScoupApplication {
@@ -12,4 +21,22 @@ public class ScoupApplication {
         SpringApplication.run(ScoupApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("Scoup")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error")))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Scoup API Documentation")
+                .version("1.0")
+                .description("Scoup의 API 문서입니다.")
+                .build();
+    }
 }
