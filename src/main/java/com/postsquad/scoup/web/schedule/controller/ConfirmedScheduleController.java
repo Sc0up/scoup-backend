9324ca6a72e106e488e15194d1491d2aeb0c731a
Dae-Hwa <daehwa_jeong@outlook.com>
2021-11-24 22:13:48 +0900

    feat: 🎸 확정된 일정 기간 조회 목업 API 및 문서 작성
    ✅ Closes: #219

--- src/main/java/com/postsquad/scoup/web/schedule/controller/ConfirmedScheduleController.java
+++ src/main/java/com/postsquad/scoup/web/schedule/controller/ConfirmedScheduleController.java
@@ -1,11 +1,16 @@
 package com.postsquad.scoup.web.schedule.controller;
 
+import com.postsquad.scoup.web.schedule.controller.request.ConfirmedSchedulePeriodReadAllRequest;
+import com.postsquad.scoup.web.schedule.controller.response.ConfirmedSchedulePeriodReadAllResponse;
 import com.postsquad.scoup.web.schedule.controller.response.ConfirmedScheduleReadAllResponses;
 import com.postsquad.scoup.web.schedule.service.ConfirmedScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +21,15 @@ public class ConfirmedScheduleController {
     @GetMapping("/groups/{groupId}/confirmed-schedules")
     public ConfirmedScheduleReadAllResponses readConfirmedSchedules(@PathVariable Long groupId) {
         return confirmedScheduleService.readAll(groupId);
+    }
+
+    @GetMapping("/groups/{groupId}/existing-schedules")
+    public List<ConfirmedSchedulePeriodReadAllResponse> readConfirmedSchedulePeriods(@PathVariable Long groupId, ConfirmedSchedulePeriodReadAllRequest confirmedSchedulePeriodReadAllRequest) {
+        return List.of(
+                ConfirmedSchedulePeriodReadAllResponse.builder()
+                                                      .start_date_time(LocalDateTime.of(2021, 11, 25, 0, 0))
+                                                      .end_date_time(LocalDateTime.of(2021, 11, 26, 0, 0))
+                                                      .build()
+        );
     }
 }
---
+++ src/main/java/com/postsquad/scoup/web/schedule/controller/response/ConfirmedSchedulePeriodReadAllResponse.java
@@ -0,0 +1,19 @@
+package com.postsquad.scoup.web.schedule.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ConfirmedSchedulePeriodReadAllResponse {
+
+    private LocalDateTime start_date_time;
+
+    private LocalDateTime end_date_time;
+}
