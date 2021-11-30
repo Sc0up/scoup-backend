40e65f0a791203f4b1d6baac64df15a84915e58a
Taeun Kim <min27604@gmail.com>
2021-11-30 13:38:36 +0900

    feat: 🎸 로그인된 사용자가 가입된 그룹 목록을 조회하는 기능 구현
    ✅ Closes: #226

--- src/main/java/com/postsquad/scoup/web/group/service/GroupService.java
+++ src/main/java/com/postsquad/scoup/web/group/service/GroupService.java
@@ -3,21 +3,27 @@ package com.postsquad.scoup.web.group.service;
 import com.postsquad.scoup.web.common.DefaultPostResponse;
 import com.postsquad.scoup.web.group.controller.request.GroupCreationRequest;
 import com.postsquad.scoup.web.group.controller.request.GroupModificationRequest;
+import com.postsquad.scoup.web.group.controller.response.GroupReadAllResponses;
 import com.postsquad.scoup.web.group.domain.Group;
 import com.postsquad.scoup.web.group.exception.GroupNameAlreadyExistException;
 import com.postsquad.scoup.web.group.exception.GroupNotFoundException;
 import com.postsquad.scoup.web.group.mapper.GroupMapper;
 import com.postsquad.scoup.web.group.repository.GroupRepository;
 import com.postsquad.scoup.web.signin.exception.UnauthorizedUserException;
+import com.postsquad.scoup.web.signin.exception.UserNotFoundException;
 import com.postsquad.scoup.web.user.domain.User;
+import com.postsquad.scoup.web.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class GroupService {
 
     private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
 
     public DefaultPostResponse create(GroupCreationRequest groupCreationRequest, User user) {
 
@@ -41,5 +47,16 @@ public class GroupService {
             throw new UnauthorizedUserException();
         }
         return groupRepository.save(group.update(groupModificationRequest)).getId();
+    }
+
+    public GroupReadAllResponses readAllByUser(User user) {
+        User loggedInUser = userRepository.findById(user.getId()).orElseThrow(() -> new UserNotFoundException(user.getId()));
+        return GroupReadAllResponses
+                .builder()
+                .groupReadAllResponse(
+                        loggedInUser.getJoinedGroups().stream()
+                                .map(GroupMapper.INSTANCE::map)
+                                .collect(Collectors.toList()))
+                .build();
     }
 }
